b22034e16a8b50c03d37a9beed212d8dfd6f8d4d
yaoyingtao <yaoyingtao@changba.com>
2019-04-22 18:41:00 +0800

    完成chapter 1

---
+++ Mywork/Chapter01/HelloAVF_Starter/Hello AV Foundation/THSpeechController.m
@@ -0,0 +1,74 @@
+//
+//  MIT License
+//
+//  Copyright (c) 2014 Bob McCune http://bobmccune.com/
+//  Copyright (c) 2014 TapHarmonic, LLC http://tapharmonic.com/
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "THSpeechController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface THSpeechController ()
+@property (nonatomic, strong) AVSpeechSynthesizer *synthesizer;
+@property (nonatomic, strong) NSArray *voices;
+@property (nonatomic, strong) NSArray *speechStrings;
+@end
+
+@implementation THSpeechController
+
++ (instancetype)speechController {
+    return [[self alloc] init];
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _synthesizer = [[AVSpeechSynthesizer alloc] init];
+        _voices = @[[AVSpeechSynthesisVoice voiceWithLanguage:@"en-US"],
+                    [AVSpeechSynthesisVoice voiceWithLanguage:@"en-GB"]];
+        _speechStrings = [self buildSpeechStrings];
+    }
+    return self;
+}
+
+- (NSArray *)buildSpeechStrings {
+    return @[@"Hello AV Foundation. How are you?",
+             @"I'm well! Thanks for asking.",
+             @"Are you excited about the book?",
+             @"Very! I have always felt so misunderstood",
+             @"What's your favorite feature?",
+             @"Oh, they're all my babies. i couldn't possibly choose.",
+             @"It was great to speak with you!",
+             @"The pleasure was all mine! Have fun!"];
+}
+
+- (void)beginConversation {
+    for (NSInteger i = 0; i < self.speechStrings.count; i++) {
+        AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc] initWithString:self.speechStrings[i]];
+        utterance.voice = self.voices[i%2];
+        utterance.rate = 0.4f;
+        utterance.pitchMultiplier = 0.8f;
+        utterance.postUtteranceDelay = 0.1f;
+        [self.synthesizer speakUtterance:utterance];
+    }
+}
+
+@end
